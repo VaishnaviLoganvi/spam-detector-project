3bbb53ddf288a006dc261697694db81ef6b88c4f
Vaishnavi Loganvi <loganvivaishnavi@gmail.com>
2026-04-16 22:35:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
 
-# Load model
 model = pickle.load(open("model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
-# Home page
+history = []
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
-# Prediction route
 @app.route('/predict', methods=['POST'])
 def predict():
-    text = request.form['email']
+    message = request.form['message']
 
-    vec = vectorizer.transform([text])
-    result = model.predict(vec)
+    data = vectorizer.transform([message])
+    prediction = model.predict(data)[0]
+    probability = model.predict_proba(data)[0]
 
-    if result[0] == 1:
-        prediction = "Spam"
+    if prediction == 1:
+        result = "Spam"
+        confidence = round(probability[1]*100, 2)
     else:
-        prediction = "Not Spam"
+        result = "Not Spam"
+        confidence = round(probability[0]*100, 2)
 
-    return render_template("index.html", prediction=prediction)
+    history.append((message, result))
+
+    return render_template("index.html", result=result, confidence=confidence, history=history)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
